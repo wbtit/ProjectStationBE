b57f90c938a0f6a325701b0f8cfe561b2a547c84
Gowtham N <INT-002@WBTPL.COM>
2024-12-16 15:29:24 +0530

    assignedlit-struct-added

---
+++ src/controllers/assignedList.js
@@ -0,0 +1,31 @@
+import prisma from "../lib/prisma.js";
+import { sendResponse } from "../utils/responder.js";
+import { isValidUUID } from "../utils/isValiduuid.js";
+
+const addAssignedList=async(req,res)=>{
+    
+}
+const getAssignedList=async(req,res)=>{
+
+}
+const getAssignedListById=async(req,res)=>{
+
+}
+const updateAssignedList=async(req,res)=>{
+
+}
+const updatePatchAssignedList=async(req,res)=>{
+
+}
+const deleteAssignedList=async(req,res)=>{
+
+}
+
+export {
+    addAssignedList,
+    getAssignedListById,
+    getAssignedList,
+    updateAssignedList,
+    updatePatchAssignedList,
+    deleteAssignedList
+}
---
+++ src/routes/assigned_list/route.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import Authenticate from "../../middlewares/authenticate.js";
+import { BroadAccess } from "../../middlewares/broadaccess.js";
+
+const router =Router();
+
+export default router;
