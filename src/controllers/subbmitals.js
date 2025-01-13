8200824f91afb5329ef36e311de19085223479b9
vanamuthuV <vanamuthuvana22@gmail.com>
2025-01-13 16:05:45 +0530

    feat: submittal done

--- src/controllers/subbmitals.js
+++ src/controllers/subbmitals.js
@@ -57,5 +57,117 @@ const AddSubmitals = async (req, res) => {
   }
 };
 
+const SentSubmittals = async (req, res) => {
+  const { id } = req.user;
 
-export { AddSubmitals}
+  try {
+    const submittals = await prisma.submittals.findMany({
+      where: {
+        sender_id: id,
+      },
+      include: {
+        fabricator: true,
+        project: true,
+        recepients: true,
+        sender: true,
+      },
+    });
+
+    return sendResponse({
+      message: "Retrived all sent submittals",
+      res,
+      statusCode: 200,
+      success: true,
+      data: submittals,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return sendResponse({
+      message: error.message,
+      res,
+      statusCode: 500,
+      success: false,
+      data: null,
+    });
+  }
+};
+
+const RecievedSubmittals = async (req, res) => {
+  const { id } = req.user;
+
+  try {
+    const submittals = await prisma.submittals.findMany({
+      where: {
+        recepient_id: id,
+      },
+      include: {
+        fabricator: true,
+        project: true,
+        recepients: true,
+        sender: true,
+      },
+    });
+
+    return sendResponse({
+      message: "Retrived all recieved submittals",
+      res,
+      statusCode: 200,
+      success: true,
+      data: submittals,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return sendResponse({
+      message: error.message,
+      res,
+      statusCode: 500,
+      success: false,
+      data: null,
+    });
+  }
+};
+
+const SubmittalsSeen = async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    console.log("Invalid ID");
+    return sendResponse({
+      message: "Invalid ID",
+      res,
+      statusCode: 400,
+      success: false,
+      data: null,
+    });
+  }
+
+  try {
+    const submittals = await prisma.submittals.update({
+      where: {
+        id,
+      },
+      data: {
+        status: false,
+      },
+    });
+
+    return sendResponse({
+      message: "Submittals seen",
+      res,
+      statusCode: 200,
+      success: true,
+      data: submittals,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return sendResponse({
+      message: error.message,
+      res,
+      statusCode: 500,
+      success: false,
+      data: null,
+    });
+  }
+};
+
+export { AddSubmitals, RecievedSubmittals, SentSubmittals, SubmittalsSeen };
