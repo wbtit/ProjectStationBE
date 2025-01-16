8822e87cbf566a36578759a506f3149c82d41683
vanamuthuV <vanamuthuvana22@gmail.com>
2025-01-16 13:48:26 +0530

    feat: websokcet init

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import { getUsers } from "./src/models/userAllModel.js";
 import cors from "cors";
 import { routes } from "./src/routes/index.js";
 import { app } from "./src/app.js";
-import http from 'http';
-import WebSocket from 'ws'; // Correct WebSocket import
+import http from "http";
+import pkg from "ws"; // Correct WebSocket import
+import { WebSocketServer } from "ws";
 
 dotenv.config();
 
@@ -45,22 +46,22 @@ app.get("/getall", async (req, res) => {
 app.use("/api", routes);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server }); // Initialize WebSocket Server correctly
+const wss = new WebSocketServer({ server });
 
 const userSockets = {};
 
 // WebSocket connection setup
-wss.on('connection', (ws, req) => {
-  const userId = req.url?.split('?userId=')[1];
+wss.on("connection", (ws, req) => {
+  const userId = req.url?.split("?userId=")[1];
   if (userId) {
     userSockets[userId] = ws;
     console.log(`User ${userId} connected`);
 
-    ws.on('message', (message) => {
+    ws.on("message", (message) => {
       console.log(`Message from user ${userId}: ${message}`);
     });
 
-    ws.on('close', () => {
+    ws.on("close", () => {
       console.log(`User ${userId} disconnected`);
       delete userSockets[userId];
     });
