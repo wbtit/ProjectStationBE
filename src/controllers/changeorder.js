d0499d95f8cb4fdc6cf9060a76231412a9ca8247
Gowtham-beep <gn86923@gmail.com>
2025-01-29 13:02:32 +0530

    Fix: chnageorder timestamp

--- src/controllers/changeorder.js
+++ src/controllers/changeorder.js
@@ -1,2 +1,29 @@
 import prisma from "../lib/prisma.js";
 import { sendResponse } from "../utils/responder.js";
+
+
+const changeOrderReceived=async(req,res)=>{
+    const {id}=req.user
+
+    try{
+        if(!id){
+            return sendResponse({
+                message:"Invalid userId",
+                res,
+                statusCode:411,
+                success:false,
+                data:null
+            })
+        }
+        const receives= await prisma.changeOrder.findMany({
+            where:{
+                recipients:id
+            },
+            select:{
+                remarks:true,
+                description:true,
+                changeOrder:true
+            }
+        })
+    }
+}
