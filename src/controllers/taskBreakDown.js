18ea986ee34c52625d37430ecbc83f700f02c6df
Gowtham-beep <gn86923@gmail.com>
2025-01-30 13:27:30 +0530

    Feat:taskbreakdown routehandler

---
+++ src/controllers/taskBreakDown.js
@@ -0,0 +1,123 @@
+import prisma from "../lib/prisma.js"
+import { sendResponse } from "../utils/responder.js"
+
+const addTaskBreakDown=async(req,res)=>{
+ const {description,QtyNo,execTime,checkingHour,projectId}=req?.body
+
+ try{
+    if(!description ||!QtyNo||!execTime||!checkingHour||!projectId){
+        return sendResponse({
+            message:"Empty inputs",
+            res,
+            statusCode:400,
+            success:false,
+            data:null
+        })
+    }
+    const newtaskBreakDown= await prisma.taskBreakdown.create({
+        description,
+        QtyNo,
+        execTime,
+        checkingHour,
+        projectId
+    })
+    if(!newtaskBreakDown){
+        return sendResponse({
+            message:"Failed to create newTaskBreakDown",
+            res,
+            statusCode:400,
+            data:null,
+            success:false
+        })
+    }
+    return sendResponse({
+        message:"taskBreakDown Created Successfully",
+        res,
+        statusCode:200,
+        success:true,
+        data:newtaskBreakDown
+    })
+
+ }catch(error){
+    return sendResponse({
+        message:error.message,
+        res,
+        statusCode:500,
+        success:false,
+        data:null
+        
+    })
+ }
+}
+
+const getTaskBreakDown=async(req,res)=>{
+    try{
+        const gettaskBreakDown= await prisma.taskBreakdown.findMany()
+
+        if(!gettaskBreakDown){
+            return sendResponse({
+                message:"failed to fetch TaskBreakDowns",
+                res,
+                statusCode:400,
+                success:false,
+                data:null
+            })
+        }
+        return sendResponse({
+            message:"TaskBreakDowns fetched successfully",
+            res,
+            statusCode:200,
+            success:true,
+            data:gettaskBreakDown
+        })
+    }catch(error){
+        return sendResponse({
+            message:error.message,
+            res,
+            statusCode:500,
+            success:false,
+            data:null
+        })
+    }
+}
+
+const putTaskBreakDown=async(req,res)=>{
+    const{id}=req?.params
+    if(!id){
+        return sendResponse({
+            message:"Id not provided",
+            res,
+            statusCode:400,
+            success:false,
+            data:null
+        })
+    }
+    try {
+    const puttaskBreakDown= await prisma.taskBreakdown.update({where:{id},data:rer.body})
+    if(!puttaskBreakDown){
+        return sendResponse({
+            message:"Failed to update",
+            res,
+            statusCode:400,
+            success:false,
+            data:null
+        })
+    }
+    return sendResponse({
+        message:"Updated successfully",
+        res,
+        statusCode:200,
+        success:true,
+        data:puttaskBreakDown
+    })
+    } catch (error) {
+        return sendResponse({
+            message:error.message,
+            res,
+            statusCode:500,
+            success:false,
+            data:null
+        })
+    }
+}
+export {addTaskBreakDown,getTaskBreakDown,putTaskBreakDown}
--- src/routes/workbreakdown/route.js
+++ src/routes/workbreakdown/route.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import Authenticate from "../../middlewares/authenticate.js";
 
+import {addTaskBreakDown,getTaskBreakDown,putTaskBreakDown} from '../../controllers/taskBreakDown.js'
+
 const router =Router
 
 router.post("/addJobStudy",Authenticate,addJobStudy)
@@ -13,7 +15,7 @@ router.get("/getSubTasks",Authenticate,getSubTasks)
 router.get("/getJobStudy",Authenticate,getJobStudy)
 
 router.put("/putJobStudy",Authenticate,putJobStudy)
-router.put("/putTaskBreakDown",Authenticate,putTaskBreakDown)
+router.put("/putTaskBreakDown/:id",Authenticate,putTaskBreakDown)
 router.put("/putSubTasks",Authenticate,putSubTasksput)
 
 export default router
