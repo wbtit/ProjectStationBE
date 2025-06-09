4f95ea5485cb15e664e27faa3202eb4dfa7d7716
Gowtham-beep <gn86923@gmail.com>
2025-06-09 16:13:51 +0530

    Feat:added deleteScripy for subtasks and wbsactivities

---
+++ Supportscripts/deletedata.js
@@ -0,0 +1,17 @@
+import prisma from "../src/lib/prisma";
+
+async function deleteAllData() {
+  try {
+    const deletedSubTasks = await prisma.subTasks.deleteMany();
+    console.log(`Deleted ${deletedSubTasks.count} SubTasks`);
+
+    const deletedWBSActivities = await prisma.wBSActivity.deleteMany();
+    console.log(`Deleted ${deletedWBSActivities.count} WBSActivities`);
+  } catch (error) {
+    console.error("Error deleting data:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+deleteAllData();
