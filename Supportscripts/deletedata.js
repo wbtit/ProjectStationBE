d12c498e23d57f5f3e9d42910b46198a28a86992
Gowtham-beep <gn86923@gmail.com>
2025-06-09 17:13:31 +0530

    Fix:try_01 CD-9-6-2025

--- Supportscripts/deletedata.js
+++ Supportscripts/deletedata.js
@@ -3,10 +3,10 @@ import prisma from "../src/lib/prisma.js";
 async function deleteAllData() {
   try {
     const deletedSubTasks = await prisma.subTasks.deleteMany();
-    console.log(`Deleted ${deletedSubTasks.count} SubTasks`);
+    console.log(`Deleted ${deletedSubTasks.count} SubTasks success`);
 
     const deletedWBSActivities = await prisma.wBSActivity.deleteMany();
-    console.log(`Deleted ${deletedWBSActivities.count} WBSActivities`);
+    console.log(`Deleted ${deletedWBSActivities.count} WBSActivities success`);
   } catch (error) {
     console.error("Error deleting data:", error);
   } finally {
