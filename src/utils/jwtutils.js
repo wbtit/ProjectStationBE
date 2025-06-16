a19d115ef83c95735e644f2831c4d1404d712613
Gowtham-beep <gn86923@gmail.com>
2025-06-16 16:02:20 +0530

    fix:token expiration

--- src/utils/jwtutils.js
+++ src/utils/jwtutils.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 export const generateToken = (payload)=> {
     (payload)
     const secretKey = process.env.SECRET; // Assuming process.env.SECRET is defined
-    const options = { expiresIn: '5h' };  // Defining the options type explicitly
+    const options = { expiresIn: '10h' };  // Defining the options type explicitly
 
     const token = jwt.sign(payload, secretKey, options);
     return token;
