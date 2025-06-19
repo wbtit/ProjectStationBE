191cd29eff3959e0aded39e3c9ebc398488c5c08
Gowtham-beep <gn86923@gmail.com>
2025-06-19 17:23:42 +0530

    Feat:estimationLineItems data added

---
+++ data/estimationLineItems.js
@@ -0,0 +1,328 @@
+const lineItems = [
+  {
+    scopeOfWork: "Column",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Beams",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.25,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Curved Beam/Rolled Beam",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 2.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Truss",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 6,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Horizontal Brace",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Vertical. Brace",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Kicker/Knee Brace",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Girt & Purlin",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Lintel/Brick Veneer Supports",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Posts/Pipe",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Hangers",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Deck edge Angle / Bent plate",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.25,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Misc. Attachments",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.25,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Outriggers",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Sag rod",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Turn Buckle",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Dock Levellers",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 4,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "RTU Frame/Roof Hatch Frame",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Roof Drain Frame",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Embed Plate/Bearing Plates",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 2,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Embeds Angle",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Stair",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 8,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Dock Stair",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 3,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Guardrail",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 2,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Handrail/Wallrails",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Landing Frame",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Roof Ladder",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 3,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Ship Ladder/Cage Ladder",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 4,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Pit Ladder",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Bollard",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Gates",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 6,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Grating",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Bike Rack",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Joist",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 0.5,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Wall",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 1,
+    totalHours: 0,
+    weeks: null
+  },
+  {
+    scopeOfWork: "Other trades",
+      
+    remarks: "",
+    quantity: null,
+    hoursPerQty: 0.75,
+    totalHours: 0,
+    weeks: null
+  }
+];
+
+export default lineItems;
