4ae9f2afd4f940bf37e467d8ae3c1ed973378094
Gowtham-beep <gn86923@gmail.com>
2025-06-23 18:53:06 +0530

    fix:create line items function corrected with the logic

--- src/utils/createEstimationLineItems.js
+++ src/utils/createEstimationLineItems.js
@@ -1,26 +1,29 @@
 import lineItems from "../../data/estimationLineItems.js";
 import prisma from "../lib/prisma.js";
 
-
-async function createEstimationLineItem(estimationId){
-    if(!estimationId){
-        console.error('estimationId is not provided')
+async function createEstimationLineItem(estimationId) {
+    if (!estimationId) {
+        console.error('❌ estimationId is not provided');
+        return;
     }
-    try {
-    const lineItemsToInsert= lineItems.map(item=>({
-        ...item &&
-        estimationId
-    }))
-    const createItems= await prisma.estimationLineItem.createMany({
-        data:lineItemsToInsert,
-        EstimationLineItem:true
-    })
-     console.log("✅ Line items inserted:", createItems);
-    return createItems;
 
-} catch (error) {
-    console.error("❌ Error inserting line items:", error);
-    throw error;   
+    try {
+        const lineItemsToInsert = lineItems.map(item => ({
+            ...item,
+            estimationId:estimationId
+        }));
+
+        const createItems = await prisma.estimationLineItem.createMany({
+            data: lineItemsToInsert
+        });
+
+        console.log("✅ Line items inserted:", createItems);
+        return createItems;
+
+    } catch (error) {
+        console.error("❌ Error inserting line items:", error);
+        throw error;
+    }
 }
-}
+
 export default createEstimationLineItem;
